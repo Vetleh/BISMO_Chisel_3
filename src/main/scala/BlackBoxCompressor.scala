590ffd2c4256320394467e4c7263bc2000f71858
Vetle Harnes <vetlewh@gmail.com>
2022-09-21 12:06:03 +0200

    Tried to get V2 working

---
+++ src/main/scala/BlackBoxCompressor.scala
@@ -0,0 +1,169 @@
+// Copyright (c) 2019 Xilinx
+//
+// BSD v3 License
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BISMO nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import Chisel._
+
+// wraps the FPGA-optimized VHDL compressor generator originally developed by
+// Thomas Preusser. although the generator supports multi-bit operands, we only
+// use it for regular binary operands here.
+
+class BlackBoxCompressorParams(
+  val N: Int, // bitwidth of compressor inputs
+  val D: Int, // number of pipeline registers, subject to
+  val WD: Int = 1, // input operand 1 precision
+  val WC: Int = 1 // input operand 2 precision
+// compressor tree depth. set to -1 for maximum.
+) extends PrintableParam {
+  def headersAsList(): List[String] = {
+    return List("Dk", "WA", "WB", "BBCompressorLatency")
+  }
+
+  def contentAsList(): List[String] = {
+    return List(N, WD, WC, getLatency()).map(_.toString)
+  }
+
+  // current compressor tree depth generated for a few values of N.
+  val depthMap = Map(32 -> 2, 64 -> 3, 128 -> 3, 256 -> 4, 512 -> 5)
+
+  def getLatency(): Int = {
+    if (D == -1) {
+      if (depthMap.contains(N)) {
+        return depthMap(N)
+      } else {
+        println(s"WARNING BlackBoxCompressor: Depth for N=$N not precomputed, defaulting to 0")
+        return 0
+      }
+    } else {
+      return D
+    }
+  }
+}
+
+// // Chisel Module wrapper around generated compressor
+// class BlackBoxCompressor(p: BlackBoxCompressorParams) extends Module {
+//   def outputbits = log2Up(p.N) + 1
+//   val io = IO(new Bundle {
+//     val c = Input(Bits(width = p.N.W))
+//     val d = Input(Bits(width = p.N.W))
+//     val r = Output(Bits(width = outputbits.W))
+//     // val c = Bits(INPUT, width = p.N)
+//     // val d = Bits(INPUT, width = p.N)
+//     // val r = Bits(OUTPUT, width = outputbits)
+//   })
+//   val inst = Module(new mac(
+//     BB_WA = outputbits, BB_N = p.N, BB_WD = p.WD, BB_WC = p.WC,
+//     BB_D = p.getLatency())).io
+//   inst.a := 0.U
+//   inst <> io
+// }
+
+// synthesizable model of BlackBoxCompressor
+class BlackBoxCompressorModel(p: BlackBoxCompressorParams) extends Module {
+  def outputbits = log2Up(p.N) + 1
+  val io = IO(new Bundle {
+    val c = Input(Bits(width = p.N.W))
+    val d = Input(Bits(width = p.N.W))
+    val r = Output(Bits(width = p.N.W))
+    // val c = Bits(INPUT, width = p.N)
+    // val d = Bits(INPUT, width = p.N)
+    // val r = Bits(OUTPUT, width = outputbits)
+  })
+  io.r := ShiftRegister(PopCount(io.c & io.d), p.getLatency())
+}
+
+// actual BlackBox that instantiates the VHDL unit
+// class mac(
+//   BB_WA: Int, // result precision
+//   BB_N: Int, // number of elements in dot product
+//   BB_WD: Int, // input operand 1 precision
+//   BB_WC: Int, // input operand 2 precision
+//   BB_D: Int // optional pipeline regs to add
+// ) extends BlackBox(Map("WA" -> BB_WA, // Verilog parameters
+//                         "N" -> BB_N,
+//                         "WD" -> BB_WD,
+//                         "WC" -> BB_WC,
+//                         "DEPTH" -> BB_D
+//                      )) {
+//   val io = new Bundle {
+//     // accumulator input is unused
+//     val a = Input(Bits(width = BB_WA.W))
+//     // val a = Bits(INPUT, width = BB_WA)
+//     // c and d are the inputs to the binary dot product
+//     val c = Input(Bits(width = (BB_N * BB_WC).W))
+//     val d = Input(Bits(width = (BB_N * BB_WC).W))
+//     // val c = Bits(INPUT, width = BB_N * BB_WC)
+//     // val d = Bits(INPUT, width = BB_N * BB_WD)
+//     // r contains the result after D cycles
+//     val r = Output(Bits(width = BB_WA.W))
+//     // val r = Bits(OUTPUT, width = BB_WA)
+//   }
+//   // setVerilogParameters(new VerilogParameters {
+//   //   val WA: Int = BB_WA
+//   //   val N: Int = BB_N
+//   //   val WD: Int = BB_WD
+//   //   val WC: Int = BB_WC
+//   //   val DEPTH: Int = BB_D
+//   // })
+
+//   // clock needs to be added manually to BlackBox
+//   // addClock(Driver.implicitClock)
+
+//   // Behavioral model for compressor: delayed AND-popcount
+//   if (BB_WD == 1 && BB_WC == 1) {
+//     io.r := ShiftRegister(PopCount(io.c & io.d), BB_D)
+//   }
+// }
+
+// Chisel Module wrapper around generated compressor
+// class CharacterizationBBCompressor(p: BlackBoxCompressorParams) extends Module {
+//   //def outputbits = log2Up(p.N) + 1
+//   val io = IO(new Bundle {
+//     val c = Input(Bits(width = p.N.W))
+//     val d = Input(Bits(width = p.N.W))
+//     val r = Input(Bits(width = 32.W))
+//     val clean = Input(Bool())
+//   })
+//   val inst = Module(new BlackBoxCompressor(p)).io
+
+//   val cReg = RegNext(io.c)
+//   inst.c := cReg
+//   val dReg = RegNext(io.d)
+//   inst.d := dReg
+
+//   val rReg = RegInit(0.U(32.W))
+//   when(io.clean) {
+//     rReg := 0.U
+//   }.otherwise {
+//     rReg := rReg + inst.r
+//   }
+//   io.r := rReg
+// }
---
+++ src/main/scala/DotProductUnit.scala
@@ -0,0 +1,157 @@
+// Copyright (c) 2018 Norwegian University of Science and Technology (NTNU)
+// Copyright (c) 2019 Xilinx
+//
+// BSD v3 License
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BISMO nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import Chisel._
+
+abstract class PrintableParam() {
+  def headersAsList(): List[String] // parameter names as a list of strings
+  def contentAsList(): List[String] // parameter values as list of strings
+}
+
+
+class DotProductUnitParams(
+  // width of inputs
+  val inpWidth: Int,
+  // width of accumulator register
+  val accWidth: Int,
+  // number of regs for the VHDL compressor
+  // -1 gives maximum pipelining (= compressor tree depth)
+  val vhdlCompressorRegs: Int = -1,
+  // whether to add an input register at the DPU input
+  val doNotRegisterInput: Boolean = false,
+  // use an optimized VHDL compressor generator
+  val useVhdlCompressor: Boolean = true
+) extends PrintableParam {
+  // parameters for BlackBoxCompressor (if any)
+  val bbCompParams = new BlackBoxCompressorParams(
+    N = inpWidth, D = vhdlCompressorRegs
+  )
+  // internal pipeline registers inside DPU
+  val internalPipelineRegs = if(doNotRegisterInput) {2} else {3}
+  val myLatency = internalPipelineRegs + bbCompParams.getLatency()
+
+  // return total latency
+  def getLatency(): Int = {
+    return myLatency
+  }
+
+  def headersAsList(): List[String] = {
+    return List("InpWidth", "AccWidth", "Latency")
+  }
+
+  def contentAsList(): List[String] = {
+    return List(inpWidth, accWidth, getLatency()).map(_.toString)
+  }
+}
+
+class NewDotProductStage0(p: DotProductUnitParams) extends Bundle {
+  // bit vectors for dot product
+  val a = Bits(width = p.inpWidth.W)
+  val b = Bits(width = p.inpWidth.W)
+  // accumulation mode
+  val acc_shift = Bool()
+  // negate contribution before accumulating
+  val neg = Bool()
+  // zero accumulator before adding
+  val clear = Bool()
+
+}
+
+class NewDotProductStage1(p: DotProductUnitParams) extends Bundle {
+  // result of AND of inputs
+  val popcountResult = UInt(width = log2Up(p.inpWidth+1).W)
+  // accumulation mode
+  val acc_shift = Bool()
+  // negate contribution before accumulating
+  val neg = Bool()
+  // zero accumulator before adding
+  val clear = Bool()
+
+}
+
+class DotProductUnit(val p: DotProductUnitParams) extends Module {
+  val io = IO(new Bundle {
+    // CHANGED
+    val in = Input(Valid(new NewDotProductStage0(p)))
+    val in_test = Input(new NewDotProductStage1(p))
+    val in_test2 = Input(Bool())
+    // val in = Valid(new NewDotProductStage0(p)).asInput
+    val out = Output(SInt(width = p.accWidth.W))
+    // val out = UInt(OUTPUT, width = p.accWidth)
+  })
+  // instantiate the compressor
+  val compressor = Module(
+    // if(p.useVhdlCompressor) {new BlackBoxCompressor(p.bbCompParams)}
+    // else {new BlackBoxCompressorModel(p.bbCompParams)}
+    new BlackBoxCompressorModel(p.bbCompParams)
+  )
+  val compLatency = p.bbCompParams.getLatency()
+  //when(io.in.valid) { printf("Input: a %x b %x shift %d neg %d clear %d\n", io.in.bits.a, io.in.bits.b, io.in.bits.acc_shift, io.in.bits.neg, io.in.bits.clear)}
+  // pipeline stage 1: compressor
+  val stage1_b = io.in_test
+  val stage1_v = io.in_test2
+
+  stage1_b.popcountResult := compressor.io.r
+  if(p.doNotRegisterInput) {
+    // give input directly to compressor
+    compressor.io.c := io.in.bits.a
+    compressor.io.d := io.in.bits.b
+    stage1_b.acc_shift := ShiftRegister(io.in.bits.acc_shift, compLatency)
+    stage1_b.neg := ShiftRegister(io.in.bits.neg, compLatency)
+    stage1_b.clear := ShiftRegister(io.in.bits.clear, compLatency)
+    stage1_v := ShiftRegister(io.in.valid, compLatency)
+  } else {
+    // pipeline stage 0: register the input
+    val regStage0_v = RegNext(io.in.valid, false.B)
+    val regStage0_b = RegNext(io.in.bits)
+    compressor.io.c := regStage0_b.a
+    compressor.io.d := regStage0_b.b
+    stage1_b.acc_shift := ShiftRegister(regStage0_b.acc_shift, compLatency)
+    stage1_b.neg := ShiftRegister(regStage0_b.neg, compLatency)
+    stage1_b.clear := ShiftRegister(regStage0_b.clear, compLatency)
+    stage1_v := ShiftRegister(regStage0_v, compLatency)
+  }
+  val regStage1_b = RegNext(stage1_b)
+  val regStage1_v = RegNext(stage1_v)
+  // pipeline stage 2: accumulate according to mode
+  val regAcc = Reg(SInt(width = p.accWidth.W))
+  // accumulate new input when valid
+  when(regStage1_v) {
+    val acc_modes = Vec(Seq[SInt](regAcc, regAcc << 1))
+    val acc = acc_modes(regStage1_b.acc_shift)
+    val contr = regStage1_b.popcountResult.zext()
+    regAcc := Mux(regStage1_b.clear, 5.S(width = p.accWidth.W), acc) + Mux(regStage1_b.neg, -contr, contr)
+    //printf("Accumulate: regAcc = %d shiftAcc? %d clear? %d contr %d \n", regAcc, regStage1_b.acc_shift, regStage1_b.clear, contr)
+  }
+  // expose the accumulator output directly
+  io.out := regAcc
+}
